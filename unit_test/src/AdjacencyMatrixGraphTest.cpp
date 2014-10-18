76680eef2da1ea8a0f000efa6b5a3f39688ddf4b
Quepas <oneinchman.dev@gmail.com>
2014-10-18 23:42:43 +0200

    Adding unit test. Updating travis/cmake.

---
+++ unit_test/src/AdjacencyMatrixGraphTest.cpp
@@ -0,0 +1,20 @@
+#include <catch.hpp>
+
+#include "AdjacencyMatrixGraph.h"
+
+using Graphene::AdjacencyMatrixGraph;
+
+TEST_CASE("Basic graph manipulation") {
+  SECTION("Adding and removing nodes") {
+    AdjacencyMatrixGraph graph;
+
+    REQUIRE(graph.GetNodeCount() == 0);
+    graph.AddNode(1);
+    graph.AddNode(2);
+    graph.AddNode(3);
+    REQUIRE(graph.GetNodeCount() == 3);
+    graph.RemoveNode(1);
+    graph.RemoveNode(4);
+    REQUIRE(graph.GetNodeCount() == 2);
+  }
+}
