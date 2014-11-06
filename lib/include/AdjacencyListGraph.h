13055889ebf2c1d16793a0142cf17a6c78844a57
Quepas <oneinchman.dev@gmail.com>
2014-11-06 13:54:51 +0100

    Adding AdjacencyListGraph class first draft.

--- lib/include/AdjacencyListGraph.h
+++ lib/include/AdjacencyListGraph.h
@@ -2,12 +2,36 @@
 
 #include "BaseGraph.h"
 
+#include <list>
+
 namespace Graphene {
+
+struct EndNode {
+  unsigned id;
+  unsigned weight;
+};
 
 class AdjacencyListGraph : public BaseGraph
 {
 public:
+  AdjacencyListGraph() {}
 
+  bool AddNode(unsigned node) override;
+  bool RemoveNode(unsigned node) override;
+  bool AddEdge(unsigned base_node, unsigned target_node, int weight) override;
+  bool RemoveEdge(unsigned base_node, unsigned target_node) override;
+
+  std::vector<unsigned> GetAdjacentNodes(unsigned node) const override;
+  std::vector<Edge> GetIncidentEdges(unsigned node) const override;
+  int GetEdgeValue(unsigned base_node, unsigned target_node) const override;
+  std::size_t GetEdgeCount() const override;
+  std::size_t GetNodeCount() const override;
+
+  bool IsNodeExsist(unsigned node) const override;
+  bool IsEdgeExsist(unsigned base_node, unsigned target_node) const override;
+
+private:
+  std::vector<std::list<EndNode>> list_;
 };
 
 }
--- lib/src/AdjacencyListGraph.cpp
+++ lib/src/AdjacencyListGraph.cpp
@@ -1,6 +1,64 @@
 #include "AdjacencyListGraph.h"
 
+using std::size_t;
+using std::vector;
+
 namespace Graphene {
+
+bool AdjacencyListGraph::AddNode(unsigned node)
+{
+  return false;
+}
+
+bool AdjacencyListGraph::RemoveNode(unsigned node)
+{
+  return false;
+}
+
+bool AdjacencyListGraph::AddEdge(unsigned base_node, unsigned target_node, int weight)
+{
+  return false;
+}
+
+bool AdjacencyListGraph::RemoveEdge(unsigned base_node, unsigned target_node)
+{
+  return false;
+}
+
+vector<unsigned> AdjacencyListGraph::GetAdjacentNodes(unsigned node) const
+{
+  return vector<unsigned>();
+}
+
+vector<Edge> AdjacencyListGraph::GetIncidentEdges(unsigned node) const
+{
+  return vector<Edge>();
+}
+
+int AdjacencyListGraph::GetEdgeValue(unsigned base_node, unsigned target_node) const
+{
+  return -1;
+}
+
+size_t AdjacencyListGraph::GetEdgeCount() const
+{
+  return -1;
+}
+
+size_t AdjacencyListGraph::GetNodeCount() const
+{
+  return -1;
+}
+
+bool AdjacencyListGraph::IsNodeExsist(unsigned node) const
+{
+  return false;
+}
+
+bool AdjacencyListGraph::IsEdgeExsist(unsigned base_node, unsigned target_node) const
+{
+  return false;
+}
 
 }
 // ~~ Graphene::AdjacencyListGraph
