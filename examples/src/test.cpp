72b5df0c71203ede26f7690ddc946de89a5156d9
Quepas <oneinchman.dev@gmail.com>
2014-11-08 00:20:42 +0100

    Adding example of loading graphs and computing its shortest paths.

--- examples/src/test.cpp
+++ examples/src/test.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 
+#include "GraphAlgorithm.h"
+#include "GraphLoader.h"
 #include "GraphInfo.h"
 #include "AdjacencyMatrixGraph.h"
+#include "AdjacencyListGraph.h"
+#include "TimeWatch.h"
 
 int main() {
   Graphene::AdjacencyMatrixGraph graph;
+  Graphene::LoadGraph("data/big_graph.txt", graph);
 
-  graph.AddNode(3);
-  graph.AddNode(6);
-  graph.AddEdge(3, 6, 22);
-  std::cout << "\nEdge 3-6: " << graph.IsEdgeExsist(3, 6);
-  std::cout << "\nEdge 6-3: " << graph.IsEdgeExsist(6, 3);
-  graph.AddEdge(6, 3, 32);
-  std::cout << "\nEdge 6-3: " << graph.IsEdgeExsist(6, 3);
-  std::cout << "\nNode 3: " << graph.IsNodeExsist(3) << std::endl;
-  graph.RemoveNode(3);
-  std::cout << "\nNode 3: " << graph.IsNodeExsist(3) << std::endl;
-  graph.AddNode(9);
-  std::cout << "Neighbours: ";
-  Graphene::PrintNodes(graph.GetAdjacentNodes(3));
+  Graphene::vector2d d;
+  Graphene::vector2d prec;
+  Graphene::TimeWatch watch;
+  Graphene::Floyd_Warshall(graph, d, prec);
 
-  Graphene::PrintAdjacencyMatrix(graph);
+  std::cout << "\n--------------- AdjacencyMatrixGraph ---------------" << std::endl;
+  std::cout << "Elapsed seconds: " << watch.Stop() << "s" << std::endl;
+  std::cout << "Distance: " << d[109][609] << std::endl;
+  Graphene::PrintPath(prec, 109, 609);
+
+  Graphene::AdjacencyListGraph graph2;
+  Graphene::LoadGraph("data/big_graph.txt", graph2);
+
+  Graphene::vector2d d2;
+  Graphene::vector2d prec2;
+  Graphene::TimeWatch watch2;
+  Graphene::Floyd_Warshall(graph2, d2, prec2);
+
+  std::cout << "\n--------------- AdjacencyListGraph ---------------" << std::endl;
+  std::cout << "Elapsed seconds: " << watch2.Stop() << "s" << std::endl;
+  std::cout << "Distance: " << d2[109][609] << std::endl;
+  Graphene::PrintPath(prec2, 109, 609);
+
 }
