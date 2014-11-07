ab7bcea1398666f6c70bdb5faf14baa0d23f4841
Quepas <oneinchman.dev@gmail.com>
2014-11-07 21:04:35 +0100

    Minor efficiency improvement in Floyd-Warshall algorithm.

--- lib/src/GraphAlgorithm.cpp
+++ lib/src/GraphAlgorithm.cpp
@@ -11,16 +11,17 @@ void Floyd_Warshall(const BaseGraph& graph, vector2d& d, vector2d& predecessors)
   auto all_nodes = graph.GetNodes();
   size_t node_count = all_nodes.size();
   auto all_edges = graph.GetEdges();
-
   ResizeVector2d(d, node_count, node_count);
   ResizeVector2d(predecessors, node_count, node_count);
 
   for (auto n1 : all_nodes) {
     for (auto n2 : all_nodes) {
-      d[n1][n2] = BaseGraph::INFINITE;
+      if (n1 != n2) 
+        d[n1][n2] = BaseGraph::INFINITE;
+      else
+        d[n1][n2] = 0;
       predecessors[n1][n2] = BaseGraph::NODE_DONT_EXSISTS;
     }
-    d[n1][n1] = 0;
   }
   for (auto edge : all_edges) {
     auto n1 = edge.base_node;
@@ -29,16 +30,15 @@ void Floyd_Warshall(const BaseGraph& graph, vector2d& d, vector2d& predecessors)
     predecessors[n1][n2] = n1;
   }
 
-  for (auto nu : all_nodes) {
-    for (auto n1 : all_nodes) {
-      for (auto n2 : all_nodes) {
+  for (unsigned nu = 0; nu < node_count; ++nu) {
+    for (unsigned n1 = 0; n1 < node_count; ++n1) {
+      for (unsigned n2 = 0; n2 < node_count; ++n2) {
         if (d[n1][n2] > d[n1][nu] + d[nu][n2]) {
           d[n1][n2] = d[n1][nu] + d[nu][n2];
           predecessors[n1][n2] = predecessors[nu][n2];
         }
       }
     }
-    std::cout << "#";
   }
 }
 
