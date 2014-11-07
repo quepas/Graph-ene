c4a97b699c97e72cea947ab4ff62fba7cfb80eba
Quepas <oneinchman.dev@gmail.com>
2014-11-07 21:03:48 +0100

    GraphLoader now realocates properly graph structure.

--- lib/include/GraphLoader.h
+++ lib/include/GraphLoader.h
@@ -13,23 +13,37 @@ bool LoadGraph(std::string path, BaseGraph& graph) {
   std::ifstream file(path);
 
   if (!file.is_open()) {
-    std::cout << "Wrong file path.";
+    std::cout << "Wrong file path." << std::endl;
     return false;
   }
 
   std::vector<Edge> edges;
   std::string line;
   int counter = 0;
+  int max_node_id = -1;
   while (std::getline(file, line)) {
+    ++counter;
     std::stringstream istream;
     int a, b, c;
     char z;
     istream << line;
+    if (line.empty()) continue;
     istream >> a >> z >>  b  >> z >> c;
-    graph.AddNode(a);
-    graph.AddNode(b);
-    graph.AddEdge(a, b, c);
+    if (a > max_node_id)
+      max_node_id = a;
+    if (b > max_node_id)
+      max_node_id = b;
+    edges.push_back({ a, b, c });
   }
+  graph.Resize(max_node_id + 1);
+  for (auto edge : edges) {
+    graph.AddNode(edge.base_node);
+    graph.AddNode(edge.target_node);
+    graph.AddEdge(edge.base_node, edge.target_node, edge.weight);
+  }
+
+  std::cout << "edges@" << path << " " << counter << std::endl;
+  std::cout << "nodes@" << path << " " << graph.GetNodeCount() << std::endl;
 }
 
 }
